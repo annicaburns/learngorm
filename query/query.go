f9b0f36e9e1311c05ac8d035b71fe93defeb0862
Annica Burns <annica.burns@healthcatalyst.com>
2017-07-07 17:26:05 -0600

    Querying: Retrieving RecordSets and using Where clauses

--- query/query.go
+++ query/query.go
@@ -21,7 +21,7 @@ import (
 
 // RetrieveSimple demonstrates some basic query language
 func RetrieveSimple() {
-
+	// Only seed the database once
 	// SeedDB()
 
 	db, err := gorm.Open("mysql", "gorm:gorm@tcp(localhost:23306)/gorm?parseTime=true")
@@ -29,9 +29,9 @@ func RetrieveSimple() {
 		panic(err.Error())
 	}
 
-	// Basic Query for first record in a table
+	// Queries for individual records
 	// Create an empty user struct
-	user := UserQuery{}
+	// user := UserQuery{}
 	// First asks Gorm to find the first record in that table (ordered ASC by primary key) and inflate that record into our empty user struct
 	// db.First(&user)
 	// FirstOrInit initializes the object provided if it doesn't find a the object provided. But it does not create the object in the database - just initializes it on the Go side.
@@ -39,8 +39,45 @@ func RetrieveSimple() {
 	// FirstOrCreate actually creates the new record in the database
 	// db.FirstOrCreate(&user, &UserQuery{Username: "lprosser"})
 	// Last asks Gorm to find the last record in the table (again ordered by primary key)
-	db.Last(&user)
-	fmt.Println(user)
+	// db.Last(&user)
+	// fmt.Println(user)
+
+	// Using the Find method to select multiple records (recordsets)
+	users := []UserQuery{}
+	// Find method can be called with just the first parameter
+	// db.Find(&users)
+	// Or with the second optional where clause parameter
+	// db.Find(&users, &UserQuery{Username: "fprefect"})
+	// Or with a map as the second parameter to choose field names at run time because this uses database field names instead of Go property names
+	// db.Find(&users, map[string]interface{}{"username": "fprefect"})
+	// And finally by using straight SQL
+	// db.Find(&users, "username = ?", "fprefect")
+
+	// Where method - Use for more complex fetches
+	// db.Where("username = ?", "fprefect").Find(&users)
+	// Can also use the Go property names or using a map as a parameter like we did with Find
+	// db.Where(&UserQuery{Username: "fprefect"}).Find(&users)
+	// db.Where(map[string]interface{}{"username": "fprefect"}).Find(&users)
+	// Can use the raw SQL method to find a group of users by passing a slice of username strings
+	// db.Where("username in (?)", []string{"adent", "mrobot"}).Find(&users)
+	// Or using Like
+	// db.Where("username like ?", "%mac%").Find(&users)
+	// Can also string together multiple elements in a where clause
+	// db.Where("username like ? and first_name = ?", "%e%", "Ford").Find(&users)
+	// Can query on time
+	// db.Where("created_at < ?", time.Now()).Find(&users)
+	// db.Where("created_at between ? and ?", time.Now().Add(-30*24*time.Hour), time.Now()).Find(&users)
+	// Not method - The Where clause is only looking for positive matches, so use the Not method for the reverse
+	// db.Not("username = ?", "adent").Find(&users)
+	// Or method. Chain this on to a where clause to combine two different fetches
+	db.Where("username = ?", "fprefect").Or("username = ?", "tmacmillan").Find(&users)
+
+	for _, user := range users {
+		// This will print the user object data but will not inflate the child calendar objects
+		// Gorm is lazy about what it loads and will only inflate child objects if explicitly requested
+		// The empty calendar object will show up after each user in the console
+		fmt.Printf("\n%v\n", user)
+	}
 }
 
 // SeedDB can be used from any package
